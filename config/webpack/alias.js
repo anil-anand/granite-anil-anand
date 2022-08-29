52f36f6ea733120323bf810a0f320fd867869d69
Anil Anand <anilanandchess@gmail.com>
2022-08-29 20:56:36 +0530

    Created Toastr component and added Axios interceptors

--- config/webpack/alias.js
+++ config/webpack/alias.js
@@ -4,6 +4,8 @@ module.exports = {
       apis: "src/apis",
       common: "src/common",
       components: "src/components",
+      utils: "src/utils",
+      constants: "src/constants",
     },
   },
 };
